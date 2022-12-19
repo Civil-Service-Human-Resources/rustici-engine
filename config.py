836465e126f58710b02add38f9b66faa0bfff2f6
jaylco <77386599+jaylco@users.noreply.github.com>
2022-12-19 13:16:49 +0000

    better error reporting for env vars

--- config.py
+++ config.py
@@ -3,7 +3,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SUBSCRIPTION_ID=os.environ['SUBSCRIPTION_ID']
+def get_required_env_var(key: str):
+	try:
+		SUBSCRIPTION_ID=os.environ[key]
+	except KeyError as e:
+		raise Exception(f"{key} is a required environment variable but was not found.")
+
+SUBSCRIPTION_ID=get_required_env_var('SUBSCRIPTION_ID')
 
 RESOURCES_DIR=f"{os.path.dirname(__file__)}/resources"
 TEMPLATES_DIR=f"{os.path.dirname(__file__)}/templates"
