16249cb60a15390cb058d58fdfc88308f71fc2aa
jaylco <77386599+jaylco@users.noreply.github.com>
2022-12-19 13:18:08 +0000

    more descriptive error for azure credentials

--- azure_credential.py
+++ azure_credential.py
@@ -1,4 +1,7 @@
 from azure.identity import DefaultAzureCredential
 
 def get_credential():
-	return DefaultAzureCredential()
+	try:
+		return DefaultAzureCredential()
+	except Exception as e:
+		raise Exception(f"Could not get Azure credentials ({e}). Are you logged into the CLI? If not are the correct environment variables set?")
