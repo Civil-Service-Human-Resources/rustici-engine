39075c2efa9c771e58aa505c76d51cd234f41f59
jaylco <77386599+jaylco@users.noreply.github.com>
2023-01-04 16:06:12 +0000

    reverted connection string vars and fixed ModernPlayCustomizationScript

--- config/installScript.sh
+++ config/installScript.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 cd /RusticiEngine/Installer
-java -Dlogback.configurationFile=logback.xml -cp "lib/*" RusticiSoftware.ScormContentPlayer.Logic.Upgrade.ConsoleApp mysql "jdbc:mysql://$DB_HOST/RusticiEngineDB?user=$DB_USER&password=$DB_PASSWORD|com.mysql.cj.jdbc.Driver"
+java -Dlogback.configurationFile=logback.xml -cp "lib/*" RusticiSoftware.ScormContentPlayer.Logic.Upgrade.ConsoleApp mysql "$CONNECTION_STRING"
 catalina.sh run
